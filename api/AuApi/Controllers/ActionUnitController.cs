646f272c710afe96860c83dd26197347467b8330
Peter Heilbo Ratgen <peter@pratgen.dk>
2022-07-08 10:47:53 +0200

    Begin AUController

---
+++ api/AuApi/Controllers/ActionUnitController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ActionUnitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost(Name = "PostVideo")]
+        public int PostVideo()
+        {
+            return 1;
+        }
+
+    }
+}
--- api/AuApi/Controllers/WeatherForecastController.cs
+++ api/AuApi/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace AuApi.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -17,11 +17,23 @@ namespace AuApi.Controllers
         {
             _logger = logger;
         }
-
+        /**
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }*/
+
+        [HttpGet(Name = "GetWeatherForecastDays")]
+        public IEnumerable<WeatherForecast> GetDays(int id)
+        {
+            return Enumerable.Range(1, id).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
